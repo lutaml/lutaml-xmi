5eb8b031694c2ec42e464265ce85d9c356a3ec55
KW Kwan <kw.kwan@ribose.com>
2024-06-19 14:01:49 +0800

    Update xmi_251_spec

--- spec/lutaml/parsers/xmi_251_spec.rb
+++ spec/lutaml/parsers/xmi_251_spec.rb
@@ -1,11 +1,30 @@
 require "spec_helper"
 
 RSpec.describe Lutaml::XMI::Parsers::XML do
-  describe '.parse' do
+  describe ".parse" do
     subject(:parse) { described_class.parse(file) }
 
-    context 'when simple xmi schema' do
-      let(:file) { File.new(fixtures_path('ea-xmi-2.5.1.xmi')) }
+    context "when parsing xmi 2013 with uml 2013" do
+      let(:file) { File.new(fixtures_path("ea-xmi-2.5.1.xmi")) }
+
+      let(:expected_first_package_package_names) do
+        [
+          "BibliographicItem",
+          nil,
+          "Block",
+          nil,
+          "ClassificationType",
+          nil,
+          "ObligationType",
+          nil,
+          "Permission",
+          "Recommendation",
+          "Requirement",
+          "RequirementSubpart",
+          nil,
+          "RequirementType",
+        ]
+      end
       let(:expected_class_names) do
         %w[
           BibliographicItem
@@ -84,6 +103,7 @@ RSpec.describe Lutaml::XMI::Parsers::XML do
           EAJava_RequirementSubpart_0..___
         ]
       end
+
       let(:expected_association_names) do
         %w[
           RequirementType
@@ -95,28 +115,18 @@ RSpec.describe Lutaml::XMI::Parsers::XML do
         expect(parse).to(be_instance_of(::Lutaml::Uml::Document))
       end
 
+      it "correctly parses model name" do
+        expect(parse.name).to(eq("EA_Model"))
+      end
+
+      it "correctly parses first package" do
+        expect(first_package.name)
+          .to(eq("requirement type class diagram"))
+      end
+
       it "correctly parses package tree" do
-        expect(parse.packages.map(&:name))
-          .to(eq(['requirement type class diagram']))
         expect(first_package.packages.map(&:name))
-          .to(eq(
-            [
-              "BibliographicItem",
-              nil,
-              "Block",
-              nil,
-              "ClassificationType",
-              nil,
-              "ObligationType",
-              nil,
-              "Permission",
-              "Recommendation",
-              "Requirement",
-              "RequirementSubpart",
-              nil,
-              "RequirementType",
-            ]
-          ))
+          .to(eq(expected_first_package_package_names))
       end
 
       it "correctly parses package classes" do
@@ -132,7 +142,7 @@ RSpec.describe Lutaml::XMI::Parsers::XML do
 
       it "correctly parses entities and attributes for class" do
         klass = first_package.classes.find do |entity|
-          entity.name == 'RequirementType'
+          entity.name == "RequirementType"
         end
 
         expect(klass.attributes.map(&:name)).to(eq(expected_attributes_names))
@@ -141,7 +151,7 @@ RSpec.describe Lutaml::XMI::Parsers::XML do
 
       it "correctly parses associations for class" do
         klass = first_package.classes.find do |entity|
-          entity.name == 'Block'
+          entity.name == "Block"
         end
 
         expect(klass.associations.map(&:member_end).compact)
@@ -152,9 +162,73 @@ RSpec.describe Lutaml::XMI::Parsers::XML do
         root_package = parse.packages.first
         expect(root_package.diagrams.length).to(eq(1))
         expect(root_package.diagrams.map(&:name))
-          .to(eq(['Starter Class Diagram']))
+          .to(eq(["Starter Class Diagram"]))
         expect(root_package.diagrams.map(&:definition))
-          .to(eq(['aada']))
+          .to(eq(["aada"]))
+      end
+    end
+  end
+
+  describe ".new" do
+    subject(:new_parser) { described_class.new }
+
+    context "when parsing xmi 2013 with uml 2013" do
+      let(:file) { File.new(fixtures_path("ea-xmi-2.5.1.xmi")) }
+
+      before do
+        xml_content = File.read(file)
+        @xmi_root_model = Xmi::Sparx::SparxRoot2013.from_xml(xml_content)
+        new_parser.send(:parse, @xmi_root_model)
+      end
+
+      it ".lookup_entity_name" do
+        owner_end = new_parser.send(:lookup_entity_name, "EAID_E50B0756_49E6_4725_AC7B_382A34BB8935")
+        expect(owner_end).to eq("verification")
+      end
+
+      it ".fetch_element" do
+        e = new_parser.send(:fetch_element, "EAID_D832D6D8_0518_43f7_9166_7A4E3E8605AA")
+        expect(e).to be_instance_of(Xmi::Sparx::SparxElement)
+        expect(e.idref).to eq("EAID_D832D6D8_0518_43f7_9166_7A4E3E8605AA")
+      end
+
+      it ".doc_node_attribute_value" do
+        val = new_parser.send(:doc_node_attribute_value, "EAID_D832D6D8_0518_43f7_9166_7A4E3E8605AA", "stereotype")
+        expect(val).to eq("Bibliography")
+
+        val = new_parser.send(:doc_node_attribute_value, "EAID_D832D6D8_0518_43f7_9166_7A4E3E8605AA", "isAbstract")
+        expect(val).to eq(false)
+
+        val = new_parser.send(:doc_node_attribute_value, "EAID_69271FAE_C52F_42ab_81B4_126CE0BF4663", "documentation")
+        expect(val).to eq("RequirementType is a generic category,&#xA;which is agnostic as to obligation.&#xA;Requirement, Recommendation, Permission&#xA;set a specific obligation, although this&#xA;can be overridden.".gsub(/&#xA;/, "\n"))
+      end
+
+
+      it ".select_all_packaged_elements" do
+        all_elements = []
+        new_parser.send(:select_all_packaged_elements, all_elements, @xmi_root_model.model, nil)
+        expect(all_elements.count).to eq(15)
+        all_elements.each do |e|
+          expect(e.is_a?(Xmi::Uml::PackagedElement) || e.is_a?(Xmi::Uml::PackagedElement2013)).to be(true)
+        end
+      end
+
+      it ".select_all_packaged_elements with type uml:Association" do
+        all_elements = []
+        new_parser.send(:select_all_packaged_elements, all_elements, @xmi_root_model.model, "uml:Association")
+        expect(all_elements.count).to eq(5)
+        all_elements.each do |e|
+          expect(e.is_a?(Xmi::Uml::PackagedElement) || e.is_a?(Xmi::Uml::PackagedElement2013)).to be(true)
+          expect(e.type).to eq("uml:Association")
+        end
+      end
+
+      it ".all_packaged_elements" do
+        all_elements = new_parser.send(:all_packaged_elements)
+        expect(all_elements.count).to eq(37)
+        all_elements.each do |e|
+          expect(e.is_a?(Xmi::Uml::PackagedElement) || e.is_a?(Xmi::Uml::PackagedElement2013)).to be(true)
+        end
       end
     end
   end
